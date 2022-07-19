efb5db3a4fa35ff11e022f00fd15e14eccc421ce
jaeh8703 <65217813+jaeh8703@users.noreply.github.com>
2022-07-19 16:59:48 +0900

    commit msg

---
+++ frontend/src/components/listers/IllnessHistoryTable.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <v-data-table
+            :headers="headers"
+            :items="illnessHistory"
+            :items-per-page="5"
+            class="elevation-1"
+    ></v-data-table>
+
+</template>
+
+<script>
+
+    export default {
+        name: 'IllnessHistoryList',
+        props: {
+            value: Object,
+            editMode: Boolean,
+            isNew: Boolean
+        },
+        data: () => ({
+            headers: [
+                { 
+                    text: "illName", 
+                    value: "illName" 
+                },
+                { 
+                    text: "start", 
+                    value: "start" 
+                },
+                { 
+                    text: "end", 
+                    value: "end" 
+                },
+            ],
+            illnessHistory : [],
+        }),
+        async created() {
+            if(!this.value) this.value = [];
+        },
+        methods: {
+        }
+    }
+</script>
+
